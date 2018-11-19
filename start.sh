ec5d100b5a1a0adf89329df12aff660b29d784c4
jiangyj <jiangyj@zjmax.com>
2018-11-19 18:16:43 +0800

    修改启动文件

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #export LD_LIBRARY_PATH=/opt/glibc2.14/lib:$LD_LIBRARY_PATH
 #go build -a -o jyj_blog main.go
 kill -9 $(pidof /var/gowww/src/jyj-go/jyj_blog)
-nohup /var/gowww/src/jyj-go/jyj_blog -c /var/gowww/src/jyj-go/conf/conf.yaml -l /var/gowww/src/jyj-go/conf/log.yaml > /var/gowww/src/jyj-go/sys.log  2>&1 &
+nohup /var/gowww/src/jyj-go/jyj_blog -c /var/gowww/src/jyj-go/conf/conf.yaml -l /var/gowww/src/jyj-go/conf/log.xml > /var/gowww/src/jyj-go/sys.log  2>&1 &
